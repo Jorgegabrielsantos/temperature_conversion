3e73d8a8e2225a2dfd61a56511d21970d14d8f2f
Jorge Gabriel <214547348+Jorgegabrielsantos@users.noreply.github.com>
2025-11-27 12:48:47 -0100

    Simple project
    A made a temperature conversion

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 temp = float(input("Enter the temperature: "))
 unit = input("In Celsius or in Fahrenheit(C/F): ")
 
-if unit == "C" or "c" or "Celsius":
-    temp = temp 
-elif == "F" or "f" or "Fahrenheit":
-    te
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    unit = "ºF"
+    print(f"In Farenheit, the temperature is: {temp} {unit}.")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    unit = "Cº"
+    print(f"In Celsius, the temperature is: {temp} {unit}.")
+else:
+    print(f"{unit} was not valid!")
