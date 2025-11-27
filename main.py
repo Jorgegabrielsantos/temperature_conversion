415b432464c574c116476573d9fd65a204e52cb6
Jorge Gabriel <214547348+Jorgegabrielsantos@users.noreply.github.com>
2025-11-27 12:37:59 -0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+temp = float(input("Enter the temperature: "))
+unit = input("In Celsius or in Fahrenheit(C/F): ")
+
+if unit == "C" or "c" or "Celsius":
+    temp = temp 
+elif == "F" or "f" or "Fahrenheit":
+    te
